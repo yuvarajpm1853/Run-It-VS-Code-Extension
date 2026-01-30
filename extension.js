8fd896758e23a5d4db1a08041c9ba17358df9c03
yuvarajp <yuvarajp@lumel.com>
2026-01-30 18:56:44 +0530

    added fn to update .env & run .sh.

--- extension.js
+++ extension.js
@@ -1,6 +1,9 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 const vscode = require('vscode');
+const fs = require('fs');
+const path = require('path');
+
 const isWindows = process.platform === 'win32';
 const lineDelimiter = isWindows ? '\r\n' : '\n';
 
@@ -24,22 +27,22 @@ function activate(context) {
 
 		// Display a message box to the user
 		vscode.window.showInformationMessage('Copied Relative Path');
-		let relativePaths = returnRelativePath(arg1, arg2)
-		if (relativePaths.length > 0) {
-			vscode.env.clipboard.writeText(relativePaths.join(lineDelimiter));
-		}
+		let relativePath = returnRelativePath(arg1, arg2)
+		if (relativePath) vscode.env.clipboard.writeText(relativePath);
 	});
+
 	const disposable1 = vscode.commands.registerCommand('run.ExecutionScripts', function (arg1, arg2) {
 		// The code you place here will be executed every time your command is executed
 
 		// Display a message box to the user
 		// vscode.window.showInformationMessage('');
 
-		let relativePaths = returnRelativePath(arg1, arg2)
-		let i = relativePaths.join(lineDelimiter)
-		console.log(i);
-
-
+		let relativePath = returnRelativePath(arg1, arg2)
+		let isUpdated = updateEnvFile(relativePath)
+		if (isUpdated) {
+			vscode.window.showInformationMessage('Updated .env');
+			runExecutionScript()
+		}
 	});
 
 	context.subscriptions.push(disposable);
@@ -66,10 +69,57 @@ function returnRelativePath(arg1, arg2) {
 				relativePaths.push(isWindows ? relativePath.replace(/\\/g, '/') : relativePath);
 			}
 		}
-		return relativePaths;
+		return relativePaths.join(lineDelimiter);
 	}
 	else return undefined
 }
+
+function updateEnvFile(testPath) {
+	const workspaceRoot = vscode.workspace.workspaceFolders?.[0]?.uri.fsPath;
+
+	if (!workspaceRoot) {
+		vscode.window.showErrorMessage('No workspace folder found');
+		return false;
+	}
+
+	const envPath = path.join(workspaceRoot, '.env');
+
+	let content = '';
+
+	if (fs.existsSync(envPath)) {
+		content = fs.readFileSync(envPath, 'utf8');
+	}
+
+	const lines = content.split(/\r?\n/);
+	let found = false;
+
+	const updatedLines = lines.map(line => {
+		if (line.startsWith('TESTNAME=')) {
+			found = true;
+			return `TESTNAME=${testPath}`;
+		}
+		return line;
+	});
+
+	if (!found) {
+		updatedLines.push(`TESTNAME=${testPath}`);
+	}
+
+	fs.writeFileSync(envPath, updatedLines.join('\n'), 'utf8');
+	return true;
+}
+
+function runExecutionScript() {
+	const terminal = vscode.window.createTerminal('Execution Scripts');
+	//   terminal.show();
+
+	// For Windows Git Bash / WSL users:
+	terminal.sendText('executeScripts.sh');
+
+	// If you're sure it's executable on Linux/macOS:
+	// terminal.sendText('./executionScripts.sh');
+}
+
 // This method is called when your extension is deactivated
 function deactivate() { }
 
